81b5d606551a5d6389028f4fce46de2b8b10bd92
Pnoo <paul@pn-software.dev>
2022-12-03 16:22:08 +0000

    day 1

--- src/advent_of_code_2022/day1.clj
+++ src/advent_of_code_2022/day1.clj
@@ -1,3 +1,31 @@
-(ns advent-of-code-2022.day1)
+(ns advent-of-code-2022.day1
+  (:require [advent-of-code-2022.core :as core]))
 
-(defn run [])
+
+(def input-file "resources/2022-day1.txt")
+
+
+(defn sum-list [l]
+  (reduce
+    +
+    (map bigdec l)))
+
+
+(defn part1 []
+  (->> input-file
+       (core/load-split) ;; load file and split by line
+       (partition-by #(= "" %)) ;; group on each empty line
+       (filter #(not= '("") %)) ;; remove empty lines
+       (map sum-list) ;; map each list of numbers to it's sum
+       (reduce max))) ;; find the max
+
+
+(defn part2 []
+  (->> input-file
+       (core/load-split) ;; load file and split by line
+       (partition-by #(= "" %)) ;; group on each empty line
+       (filter #(not= '("") %)) ;; remove empty lines
+       (map sum-list) ;; map each list of numbers to it's sum
+       (sort >) ;; sort by size
+       (take 3) ;; take 3 largest
+       (reduce +))) ;; sum them
