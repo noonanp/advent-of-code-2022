ba6eee53f7af08ba02cfd339dcfc54f2aa2444c9
Pnoo <paul@pn-software.dev>
2022-12-14 17:20:14 +0000

    day6

---
+++ src/advent_of_code_2022/day6.clj
@@ -0,0 +1,38 @@
+(ns advent-of-code-2022.day6
+  (:require [advent-of-code-2022.core :as core]
+            [clojure.string :as str]))
+
+
+(def input-file "resources/2022-day6.txt")
+
+
+(defn distinct-seq? [seq]
+  (= (count seq) (count (distinct seq)))) ;; compare the size of the seq to the size of the distinct seq
+
+
+(defn create-partitions [n input-file]
+  "Create a vector of partitions, each partition is of n characters"
+  (->> input-file
+       (core/load-split)
+       (first)
+       (partition n 1)
+       (vec)))
+
+
+(defn part1 []
+  (let [partitions (create-partitions 4 input-file)
+        first-unique (first (filter distinct-seq? partitions))]
+    (+ 4 (.indexOf partitions first-unique)))) ;; index of the first unique 4 char group + 4 as the index is the count
+                                               ;; to the beginning of the marker, and the answer is the end of the marker
+
+
+(defn part2 []
+  (let [partitions (create-partitions 14 input-file)
+        first-unique (first (filter distinct-seq? partitions))]
+    (+ 14 (.indexOf partitions first-unique)))) ;; index of the first unique 14 char group + 14 as the index is the count
+                                                ;; to the beginning of the marker, and the answer is the end of the marker
+
+
+
+
+
